163ba69f99ffb39dfc74cd1e91335203ce47f324
Johann Knechtel <johann@nyu.edu>
2019-03-04 20:37:55 +0400

    added build script; for proper and separate debug and release binaries using cmake

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "Parameters required:"
+	echo "1) Build from scratch? (y/n)"
+	exit
+fi
+
+# debug build
+mkdir -p build_debug && cd $_
+if [ $1 == "y" ]; then
+	rm -r *
+fi
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-O0 -g" ../src/
+make
+
+cd ../
+
+# regular build
+mkdir -p build && cd $_
+if [ $1 == "y" ]; then
+	rm -r *
+fi
+cmake ../src/
+make
