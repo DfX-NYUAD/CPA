1fccbf06c2fe83841bae78a2dbdc64e131c5ccca
Johann Knechtel <johann@nyu.edu>
2019-03-24 19:05:58 +0400

    added script to parse log files for number of traces needed

---
+++ data/parse_logs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $# -lt 6 ]; then
+       echo "Parameters required:"
+       echo "1) Number of steps"
+       echo "2) Number of permutations"
+       echo "3) Start step"
+       echo "4) Run ID"
+       echo "5) List of technologies, as one parameter; e.g., \"7nm 32nm 45nm SDF NCFET baseline\""
+       echo "6) Number of keys"
+       exit
+fi
+
+steps=$1
+permutations=$2
+steps_start=$3
+run=$4
+tech_list=$5
+keys=$6
+
+for tech in $tech_list
+do
+	for ((key = 1; key <= keys; key++))
+	do
+		file="*"$tech"_key_"$key"__"$steps"_steps_"$permutations"_perm_"$steps_start"_steps_start__run_"$run".log"
+
+		ls -l $file
+	done
+done
+
+for tech in $tech_list
+do
+	for ((key = 1; key <= keys; key++))
+	do
+		file="*"$tech"_key_"$key"__"$steps"_steps_"$permutations"_perm_"$steps_start"_steps_start__run_"$run".log"
+
+		echo -n `tail -n 9 $file | head -n 1 | awk '{print $1}' | cut -c 2-`"	"
+	done
+	echo
+done
