acf81976d9483b9bbcda35b252887ba10f9b92b4
Johann Knechtel <johann@nyu.edu>
2019-03-24 13:43:04 +0400

    edit dalma scripts

--- data/generate_slurm.sh
+++ data/generate_slurm.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ $# -lt 5 ]; then
+if [ $# -lt 6 ]; then
        echo "Parameters required:"
        echo "1) Number of steps for exploring subsets of traces?"
        echo "2) Number of permutations to consider for each subset of traces?"
        echo "3) Start step?"
        echo "4) Run ID?"
        echo "5) List of technologies, as one parameter; e.g., \"7nm 32nm 45nm SDF NCFET baseline\""
-       echo "6) Optional; stop step, e.g., 50"
-       echo "7) Optional; perm file, e.g., aes_5k__100_steps_10000_perm_1_steps_start.perm"
-       echo "8) Optional; rate stop, e.g., 99.99"
+       echo "5) Number of keys?"
+       echo "7) Optional; stop step, e.g., 50"
+       echo "8) Optional; perm file, e.g., aes_5k__100_steps_10000_perm_1_steps_start.perm"
+       echo "9) Optional; rate stop, e.g., 99.99"
        exit
 fi
 
@@ -19,11 +20,12 @@ permutations=$2
 steps_start=$3
 run=$4
 tech_list=$5
+keys=$6
 
 # optional parameters
-steps_stop=$6
-perm_file=$7
-rate_stop=$8
+steps_stop=$7
+perm_file=$8
+rate_stop=$9
 
 # prepare optional parameters, if provided
 if [ "$steps_stop" != "" ]; then
@@ -38,7 +40,7 @@ fi
 
 rm sbatch.sh
 
-for key in {1..10}
+for ((key = 1; key <= keys; key++))
 do
 	for tech in $tech_list
 	do
