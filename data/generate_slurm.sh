1574ceb6e6b91bc29241c0de1ca3ae8913b4295a
Johann Knechtel <johann@nyu.edu>
2019-03-24 13:31:34 +0400

    edits dalma scripts

--- data/generate_slurm.sh
+++ data/generate_slurm.sh
@@ -6,28 +6,51 @@ if [ $# -lt 5 ]; then
        echo "2) Number of permutations to consider for each subset of traces?"
        echo "3) Start step?"
        echo "4) Run ID?"
-       echo "5) List of technologies, as one parameter; e.g., \"7 32 45 SDF\""
+       echo "5) List of technologies, as one parameter; e.g., \"7nm 32nm 45nm SDF NCFET baseline\""
+       echo "6) Optional; stop step, e.g., 50"
+       echo "7) Optional; perm file, e.g., aes_5k__100_steps_10000_perm_1_steps_start.perm"
+       echo "8) Optional; rate stop, e.g., 99.99"
        exit
+fi
+
+# mandatory parameters
+steps=$1
+permutations=$2
+steps_start=$3
+run=$4
+tech_list=$5
+
+# optional parameters
+steps_stop=$6
+perm_file=$7
+rate_stop=$8
+
+# prepare optional parameters, if provided
+if [ "$steps_stop" != "" ]; then
+	steps_stop="-steps_stop $steps_stop"
+fi
+if [ "$perm_file" != "" ]; then
+	perm_file="-perm_file $perm_file"
+fi
+if [ "$rate_stop" != "" ]; then
+	rate_stop="-rate_stop $rate_stop"
 fi
 
 rm sbatch.sh
 
-perm_file=""
-#perm_file="-perm_file aes_5k__100_steps_10000_perm_1_steps_start.perm"
-
 for key in {1..10}
 do
-	for tech in $5
+	for tech in $tech_list
 	do
 		run_generic="aes_5k_key_"$key
 		if [ "$tech" == "SDF" ]; then
-			tech=65
-			run_tech="aes_5k_"$tech"nm_SDF_key_"$key
+			tech="65nm"
+			run_tech="aes_5k_"$tech"_SDF_key_"$key
 		else
-			run_tech="aes_5k_"$tech"nm_key_"$key
+			run_tech="aes_5k_"$tech"_key_"$key
 		fi
 
-		run_=$run_tech"__"$1"_steps_"$2"_perm_"$3"_steps_start__run_"$4
+		run_=$run_tech"__"$steps"_steps_"$permutations"_perm_"$steps_start"_steps_start__run_"$run
 		script=$run_".slurm"
 
 		echo "Generate $script ..."
@@ -43,16 +66,21 @@ do
 		sed_string="s,correct_key=TODO,correct_key=correct_key_"$run_generic".txt,g"
 		sed -i "$sed_string" $script
 
-		sed_string="s,steps=TODO,steps="$1",g"
+		sed_string="s,steps=TODO,steps="$steps",g"
 		sed -i "$sed_string" $script
 
-		sed_string="s,permutations=TODO,permutations="$2",g"
+		sed_string="s,permutations=TODO,permutations="$permutations",g"
 		sed -i "$sed_string" $script
 
-		sed_string="s,steps_start=TODO,steps_start="$3",g"
+		sed_string="s,steps_start=TODO,steps_start="$steps_start",g"
 		sed -i "$sed_string" $script
 
-		#sed_string="s,perm_file=TODO,perm_file="$run_".perm,g"
+		sed_string="s,steps_stop=TODO,steps_stop=\""$steps_stop\"",g"
+		sed -i "$sed_string" $script
+
+		sed_string="s,rate_stop=TODO,rate_stop=\""$rate_stop\"",g"
+		sed -i "$sed_string" $script
+
 		sed_string="s,perm_file=TODO,perm_file=\""$perm_file\"",g"
 		sed -i "$sed_string" $script
 
